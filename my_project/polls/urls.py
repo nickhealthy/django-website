a3a791035633a8f4e4f0c5c5ce1d145e97818851
nick <jobinformation@naver.com>
2021-11-20 16:19:29 +0900

    NEW: make URL_conf  APP(polls)

---
+++ my_project/polls/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path    
+from . import views
+
+app_name = 'polls' # URL 패턴의 이름이 충돌나는 것을 방지하기 위한 namespace 정의
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('polls/<int:question_id>/', views.detail, name='detail'),
+    path('polls/<int:question_id>/results', views.results, name='results'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
+]
