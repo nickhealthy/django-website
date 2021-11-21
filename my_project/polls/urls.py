3fc736b4d5b11aea068a8587c6f7bae17ead3733
nick <jobinformation@naver.com>
2021-11-21 20:44:28 +0900

    FINISHED: make VIEW & TEMPLATES - polls APP

--- my_project/polls/urls.py
+++ my_project/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'polls' # URL 패턴의 이름이 충돌나는 것을 방지하기 위한 namespace 정의
 urlpatterns = [
     path('', views.index, name='index'),
-    path('polls/<int:question_id>/', views.detail, name='detail'),
-    path('polls/<int:question_id>/results', views.results, name='results'),
-    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
